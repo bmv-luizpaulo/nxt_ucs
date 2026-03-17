5598368d29b9b5da578a0941f62b10727faac5da
Luiz Paulo Gonçalves Miguel de Jesus <luizpaulo.jesus@bmv.global>
2026-03-17 15:58:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4689357426-d2eef",
+  "appId": "1:88094033825:web:ec5e1a3c6dd6a12eb0ede0",
+  "apiKey": "AIzaSyDy-p2hl1wAij6ETR15OfvWcajc947xbis",
+  "authDomain": "studio-4689357426-d2eef.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "88094033825"
+};
